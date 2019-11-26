fc4f1982872a568d077547eb0345560157125d71
acelawrencesales <47411112+acelawrencesales@users.noreply.github.com>
2019-11-26 09:15:30 +0800

    Add files via upload

---
+++ Programing Assignment 9.1.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+data1 = {'Student': ['Ice Bear', 'Panda', 'Grizzly'], 'Math': [80, 95, 79]}
+math = pd.DataFrame(data1, columns = ['Student', 'Math'])
+data2 = {'Student': ['Ice Bear', 'Panda', 'Grizzly'], 'Electronics': [85, 81, 83]}
+electronics = pd.DataFrame(data2, columns = ['Student', 'Electronics'])
+data3 = {'Student': ['Ice Bear', 'Panda', 'Grizzly'], 'GEAS': [90, 79, 93]}
+geas = pd.DataFrame(data3, columns = ['Student', 'GEAS'])
+data4 = {'Student': ['Ice Bear', 'Panda', 'Grizzly'], 'ESAT': [93, 89, 88]}
+esat = pd.DataFrame(data4, columns = ['Student', 'ESAT'])
+
+merge = pd.merge(math, electronics, how='right', on='Student')
+merge1 = pd.merge(merge, geas, how='right', on='Student')
+merge2 = pd.merge(merge1, esat, how='right', on='Student')
+
+merged = pd.melt(merge2, id_vars = 'Student', value_vars = ['Math', 'Electronics', 'GEAS', 'ESAT'])
+merged1 = merged.rename(columns = {'variable': 'Courses', 'value': 'Grades'})
+merged2 = merged1.sort_values('Student').reset_index().drop(columns = 'index')
