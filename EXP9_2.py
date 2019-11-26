ed058cd82662b8265917de6b3e0a2de47402b851
acelawrencesales <47411112+acelawrencesales@users.noreply.github.com>
2019-11-26 16:34:11 +0800

    Add files via upload

---
+++ EXP9_2.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+boxdata = {'Box': ['Box1', 'Box1', 'Box1', 'Box2', 'Box2', 'Box2'], 'Dimension': ['Length', 'Width', 'Height', 
+           'Length', 'Width', 'Height'], 'Value': [6, 4, 2, 5, 3, 4]}
+
+box = pd.DataFrame(boxdata, columns = ['Box', 'Dimension', 'Value'])
+
+tidy = box.pivot_table(index = 'Box', columns = 'Dimension', values = 'Value').reset_index()
+
+print(tidy)
